b00076c3ab97854a5fc8cb3a94df054158896151
Lee Yoong Ti <leeyoongti@gmail.com>
2020-05-31 01:19:18 +0800

    move problems into folder

---
+++ Algorithms/Implementation/kangaroo.java
@@ -0,0 +1,15 @@
+static String kangaroo(int x1, int v1, int x2, int v2) {
+  if(v1 <= v2) {
+      return "NO";
+  } else{
+      while(x1 <= x2) {
+      if(x1 == x2){
+          return "YES";
+      }
+
+      x1 += v1;
+      x2 += v2;
+  }
+      return "NO";
+  }
+}
