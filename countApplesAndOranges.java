188b8c288ce2539cf86fc26c134a75643511ae6b
Lee Yoong Ti <leeyoongti@gmail.com>
2020-05-30 19:41:30 +0800

    add 2 problems

---
+++ countApplesAndOranges.java
@@ -0,0 +1,20 @@
+// Complete the countApplesAndOranges function below.
+    // s - t = range of Sam's house, we want to find apple and orange
+    // that fall into this range
+    // a = position of apple tree
+    // b = position of orange tree
+    static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+      int appleCount = 0;
+      int orangeCount = 0;
+      for(int apple : apples) {
+          int applePos = apple + a;
+          if(applePos >= s && applePos <= t) appleCount++;
+      }
+
+      for(int orange : oranges) {
+          int orangePos = orange + b;
+          if(orangePos >= s && orangePos <= t) orangeCount++;
+      }
+      System.out.println(appleCount);
+      System.out.println(orangeCount);
+  }
---
+++ gradingStudents.java
@@ -0,0 +1,13 @@
+public static List<Integer> gradingStudents(List<Integer> grades) {
+    // Write your code here
+    List<Integer> result = new LinkedList<Integer>();
+    for(int grade : grades) {
+        if(grade % 5 >= 3 && grade >= 38) {
+            int roundUpGrade = 5 * (grade / 5 + 1);
+            result.add(roundUpGrade);
+        } else {
+            result.add(grade);
+        }
+    }
+    return result;
+  }
